0a290f16d8aa554919afc5bd51c146c33c1d5931
Kotchaporn18 <S6506021622116@email.kmutnb.ac.th>
2024-02-12 11:18:10 +0700

    sas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://node57298-firstwebt.proen.app.ruk-com.cloud:11543";
+const base_url = "http://10.104.7.55";
 
 app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine','ejs');
