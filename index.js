867ea5161347d62e0048406b3bcdcd57cfcf2c09
Kotchaporn18 <S6506021622116@email.kmutnb.ac.th>
2024-02-12 10:52:24 +0700

    save

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const axios = require('axios');
+const path = require("path");
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://10.104.7.55";
+const base_url = "http://node57298-firstwebt.proen.app.ruk-com.cloud:11543";
 
+app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use(express.static(__dirname + "/public/views"));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/",async(req, res) => {
     try {
