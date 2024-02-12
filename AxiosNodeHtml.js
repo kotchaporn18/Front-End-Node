e79ded437d6fbf858df61f488d0a0f8b550b23ba
Kotchaporn18 <S6506021622116@email.kmutnb.ac.th>
2024-02-12 10:04:39 +0700

    save

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -9,7 +9,7 @@ app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public/views"));
 
 app.get("/",async(req, res) => {
     try {
